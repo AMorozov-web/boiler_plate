845e6cb231624baeb8ab5f0f284f31872620a43d
Alex Morozov <a.morozov.work@mail.ru>
2021-07-10 12:05:08 +0300

    refactor: refactor global styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Normalize} from 'styled-normalize';
+import {createGlobalStyle} from 'styled-components';
+import {normalize} from 'styled-normalize';
 import {App} from './components/app/app';
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+`;
 
 ReactDOM.render(
     <>
-      <Normalize />
+      <GlobalStyle />
       <App />
     </>,
     document.querySelector(`#app`)
