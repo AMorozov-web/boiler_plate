3557a1f0207de3738984da7d8d4adec2d3c5499d
Alex Morozov <a.morozov.work@mail.ru>
2021-07-10 10:55:11 +0300

    init: add base config with styled components

---
+++ src/components/app/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import 'normalize.css';
+
+const App = () => {
+
+  return (
+    <h1>My New App</h1>
+  );
+};
+
+export {App};
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'js/bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    watchContentBase: true,
+    historyApiFallback: true,
+    hot: true,
+    open: false,
+    port: 1337,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[path][name].[ext]',
+                emitFile: false,
+              },
+            },
+          ],
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src/assets/img',
+          to: 'img',
+        },
+        {
+          from: 'src/assets/fonts',
+          to: 'fonts',
+        },
+      ],
+    }),
+  ],
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), path.resolve('node_modules')],
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'inline-source-maps',
+};
